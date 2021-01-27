4a6eb5a00e1bc96740355355739b88b8250bb3f7
Thomas Retzloff <tretzloff@gmail.com>
2021-01-26 18:16:45 -0800

    added dcker compose to getdocker file

--- getDocker.sh
+++ getDocker.sh
@@ -4,3 +4,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
 sudo apt update
 sudo apt install docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
